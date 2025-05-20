6aa86a560cca8a55e2735f8b2c6f2437c7d50d02
Chien-Wu <tw.chien572@gmail.com>
2025-05-20 20:55:20 +1000

    subset_sum printing

--- subset_sum.c
+++ subset_sum.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int subsetsum(int A[], int n, int k);
+int
+subsetsum(int A[], int n, int k, int path[]) {
+	if (k==0) {
+		return 1;
+	} else if (n==0) {
+		return 0;
+	} else if (subsetsum(A, n-1, k-A[n-1], path)){
+        path[n-1] = 1;
+        return 1;
+    } else if (subsetsum(A, n-1, k, path)){
+        path[n-1] = 0;
+        return 1;
+    }
+    return 0;
+}
 
 int
 main(int argc, char *argv[]) {
@@ -11,24 +25,21 @@ main(int argc, char *argv[]) {
 	    234, 238, 241, 276, 279, 288, 386, 387, 388, 389,
 	    413, 444, 487, 513, 534, 535, 616, 722, 786, 787
 	};
-	int n=20, k=20;
-	if (argc==3) {
-		n = atoi(argv[1]);
-		k = atoi(argv[2]);
-	}
-	printf("n=%d, k=%d, subsetsum = %d\n",
-			n, k, subsetsum(A, n, k));
+
+	int n=20, k=2000;
+    int path[40] = {0};
+
+    if (subsetsum(A, n, k, path)){
+        printf("n=%d, k=%d, subsetsum = 1\n", n, k);
+    } else {
+        printf("n=%d, k=%d, subsetsum = 0\n", n, k);
+    }
+    for (int i = 0; i < 40; i++){
+        if (path[i]){
+            printf("%d ", A[i]);
+        }
+    }
+    printf("\n");
 	return 0;
 }
 
-int
-subsetsum(int A[], int n, int k) {
-	if (k==0) {
-		return 1;
-	} else if (n==0) {
-		return 0;
-	} else {
-		return subsetsum(A, n-1, k-A[n-1]) ||
-			subsetsum(A, n-1, k);
-	}
-}
